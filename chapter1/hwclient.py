cab838f93f19c91ff4789dcf07b1b788ab44b150
Manuel Šarić <manujelko@gmail.com>
2024-04-25 18:39:40 +0200

    Request reply pattern

---
+++ chapter1/hwclient.py
@@ -0,0 +1,13 @@
+import zmq
+
+context = zmq.Context()
+
+print("Connecting to hello world server...")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+for request in range(10):
+    print(f"Sending request {request} ...")
+    socket.send_string("Hello")
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
